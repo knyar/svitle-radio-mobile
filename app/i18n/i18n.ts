8ad025b1b4dec85272b1856afe5f586a78065324
Anton Tolchanov <commits@knyar.net>
2020-06-15 09:45:55 +0100

    Don't autodetect the language

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -1,4 +1,3 @@
-import * as RNLocalize from "react-native-localize"
 import i18n from "i18n-js"
 const { I18N_LANGUAGE } = require("../config/flavor")
 
@@ -7,9 +6,4 @@ const ru = require("./ru")
 
 i18n.fallbacks = true
 i18n.translations = { ua, ru }
-
-const fallback = { languageTag: I18N_LANGUAGE, isRTL: false }
-
-const { languageTag } =
-  RNLocalize.findBestAvailableLanguage(Object.keys(i18n.translations)) || fallback
-i18n.locale = languageTag
+i18n.locale = I18N_LANGUAGE
