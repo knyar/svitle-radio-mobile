b0be567667af51c98d65c1f55cda0397ed01108e
Anton Tolchanov <commits@knyar.net>
2020-07-05 12:56:55 +0100

    Use multidex

--- android/app/src/main/java/com/svitleradio/MainApplication.java
+++ android/app/src/main/java/com/svitleradio/MainApplication.java
@@ -1,7 +1,7 @@
 package com.svitleradio;
 
-import android.app.Application;
 import android.content.Context;
+import androidx.multidex.MultiDexApplication;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -10,7 +10,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
