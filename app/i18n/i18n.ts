45cbc2bd9bd4f3cbd6dd773afbc04590fb0139c6
Anton Tolchanov <commits@knyar.net>
2020-07-26 12:49:56 +0100

    Fix ukrainian translation

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -1,11 +1,11 @@
 import * as RNLocalize from "react-native-localize"
 import i18n from "i18n-js"
 
-const ua = require("./ua")
+const uk = require("./uk")
 const ru = require("./ru")
 
 i18n.fallbacks = true
-i18n.translations = { ua, ru }
+i18n.translations = { uk, ru }
 
 const fallback = { languageTag: "ru", isRTL: false }
 
