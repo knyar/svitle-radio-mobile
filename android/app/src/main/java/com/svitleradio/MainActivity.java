2abbe50a776565642229a9496c7303993d2fd66c
Anton Tolchanov <commits@knyar.net>
2020-07-26 13:05:04 +0100

    Avoid android crashes on restoring from background
    Might prevent the 'Unable to start activity ComponentInfo' errors.
    
    via https://github.com/software-mansion/react-native-screens/issues/17

--- android/app/src/main/java/com/svitleradio/MainActivity.java
+++ android/app/src/main/java/com/svitleradio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.svitleradio;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -10,5 +11,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "SvitleRadio";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
--- app/config/env.dev.js
+++ app/config/env.dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  API_URL: "http://127.0.0.1:5000/v2",
+  API_URL: "http://10.11.12.21:5000/v2",
   REFRESH_INTERVAL: 5000,  // 5 sec
 }
