93d5e76362a5bb070fcc0c79c0ddf2821034fc6d
Anton Tolchanov <commits@knyar.net>
2023-08-27 08:36:51 +0200

    upgrade - step 2

--- android/app/src/main/java/com/svitleradio/MainActivity.java
+++ android/app/src/main/java/com/svitleradio/MainActivity.java
@@ -2,6 +2,9 @@ package com.svitleradio;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
   /**
@@ -11,6 +14,20 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "SvitleRadio";
+  }
+
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
 
   @Override
