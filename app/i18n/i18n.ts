43273c10826a4728b09ee1bbff9a94ba0031d8c3
Anton Tolchanov <commits@knyar.net>
2023-09-03 16:29:47 +0100

    upgrade react-native-localize, use Ukrainian as the default language

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -7,8 +7,8 @@ const ru = require("./ru")
 i18n.fallbacks = true
 i18n.translations = { uk, ru }
 
-const fallback = { languageTag: "ru", isRTL: false }
+const fallback = { languageTag: "uk", isRTL: false }
 
 const { languageTag } =
-  RNLocalize.findBestAvailableLanguage(Object.keys(i18n.translations)) || fallback
+  RNLocalize.findBestLanguageTag(Object.keys(i18n.translations)) || fallback
 i18n.locale = languageTag
